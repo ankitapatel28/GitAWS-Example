049ab8d06a38ef453fd497ab20508d256f9bbbd2
ankitapatel28 <ankitajain2890@gmail.com>
2021-05-20 19:39:33 +0530

    Changed first time

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
       
-		System.out.println("This is Git Jenkins testing !");
+		System.out.println("This is new Git Jenkins testing !");
 
     }
 
