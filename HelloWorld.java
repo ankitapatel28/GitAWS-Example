198ec88ef9c7cf552165d20b5f69e47f1bae1183
ankitapatel28 <ankitajain2890@gmail.com>
2021-05-21 19:52:08 +0530

    This is first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
       
-		System.out.println("This is new Git Jenkins testing !");
+		System.out.println("This is second Git Jenkins testing !");
 
     }
 
